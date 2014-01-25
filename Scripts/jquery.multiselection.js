6a74591af3db7c0f3b25df2ae220b0f1c02499d8
Augustine Wen <ashramwen@gmail.com>
2014-01-25 22:37:17 +0800

    demo

---
+++ Scripts/jquery.multiselection.js
@@ -0,0 +1,9 @@
+﻿/*
+MultiSelect v1
+
+Checkbox in Select
+
+Augustine
+2013/6/20
+*/
+$.fn.multiselect=function(b){var c=$(this).find("div.list-select");$(document).click(function(){$("div.custom-select").find("div.drop-select:visible").slideUp("fast")});$(this).find("div.text-select").on("click",function(a){a.stopPropagation();$("div.text-select").not(this).next("div.drop-select:visible").slideUp("fast");$(this).next("div.drop-select").slideToggle("fast")});$(this).find("input.no-select").on("click",function(a){a.stopPropagation();$(this).parents("div.drop-select").slideToggle("fast")}); $(this).find("input.all-select").on("click",function(a){a.stopPropagation();var b=$(this).is(":checked");c.find('input[type="checkbox"]').each(function(){$(this)[0].checked=b})});$(this).find("input.option-select").on("click",function(a){a.stopPropagation();c.find('input.option-select[type="checkbox"]').each(function(){if(!$(this).is(":checked"))return c.find("input.all-select")[0].checked=!1;c.find("input.all-select")[0].checked=!0})});$(this).find("input.yes-select").on("click",function(a){a.stopPropagation(); void 0!=b&&void 0!=b.YesClick&&b.YesClick();$(this).parents("div.drop-select").slideToggle("fast")})};
